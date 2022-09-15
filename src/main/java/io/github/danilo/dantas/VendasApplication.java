0224fea9ad482e1b5685b76da7c866c43e3eae06
Danilo <danilodantasofc@gmail.com>
2022-09-15 10:40:36 -0300

    adicionando dependencias ao pom e rodando primeiro hello world

---
+++ src/main/java/io/github/danilo/dantas/VendasApplication.java
@@ -0,0 +1,21 @@
+package io.github.danilo.dantas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+	
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "hello world";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(VendasApplication.class, args);
+	}
+}
+
