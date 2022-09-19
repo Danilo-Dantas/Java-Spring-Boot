3930c1bf7702db6c1de9324a6b228a7fdd8856d1
Danilo <danilodantasofc@gmail.com>
2022-09-19 17:02:03 -0300

    configurando properties para ambiente prod, dev

--- src/main/java/io/github/danilodantas/MinhaConfiguration.java
+++ src/main/java/io/github/danilodantas/MinhaConfiguration.java
@@ -1,15 +1,18 @@
 package io.github.danilodantas;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
 
-//	@Bean(name = "applicationName")
-//	public String applicationName() {
-//		return "Sistema de Vendas";
-//	}
-	
-	
+	@Bean
+	public CommandLineRunner executar() {
+		return args -> {
+			System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+		};
+	}
 }
