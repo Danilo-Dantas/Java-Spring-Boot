5fe2689e8c40cf2d659c147d4152afcb571f9e72
Danilo <danilodantasofc@gmail.com>
2022-09-21 16:27:54 -0300

    criando classes modelo

---
+++ src/main/java/io/github/danilodantas/domain/entity/Pedido.java
@@ -0,0 +1,45 @@
+package io.github.danilodantas.domain.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido {
+
+	private Integer id;
+	private Cliente cliente;
+	private LocalDate dataPedido;
+	private BigDecimal total;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	
+	public LocalDate getDataPedido() {
+		return dataPedido;
+	}
+	public void setDataPedido(LocalDate dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+	
+	
+	public BigDecimal getTotal() {
+		return total;
+	}
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+	
+}
