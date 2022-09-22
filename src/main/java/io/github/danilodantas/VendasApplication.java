898ec522c4adb5b61b873cea7f5178f15e0bd298
Danilo <danilodantasofc@gmail.com>
2022-09-22 18:06:51 -0300

    to com erro que nao consigo arrumar >:(

--- src/main/java/io/github/danilodantas/VendasApplication.java
+++ src/main/java/io/github/danilodantas/VendasApplication.java
@@ -1,10 +1,25 @@
 package io.github.danilodantas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.github.danilodantas.domain.entity.Cliente;
+import io.github.danilodantas.domain.repositorio.Clientes;
 
 @SpringBootApplication
 public class VendasApplication {
+	
+	@Bean
+	public CommandLineRunner init(@Autowired Clientes clientes) {
+		return args -> {
+			Cliente cliente = new Cliente();
+			cliente.setNome("Danilo");
+			clientes.salvar(cliente);
+		};
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
--- src/main/java/io/github/danilodantas/domain/repositorio/Clientes.java
+++ src/main/java/io/github/danilodantas/domain/repositorio/Clientes.java
@@ -1,7 +1,12 @@
 package io.github.danilodantas.domain.repositorio;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import io.github.danilodantas.domain.entity.Cliente;
 
@@ -9,6 +14,7 @@ import io.github.danilodantas.domain.entity.Cliente;
 public class Clientes {
 
 	private static String INSERT = "insert into cliente (nome) values (?)";
+	private static String SELECT_ALL = "SELECT * FROM CLIENTE";
 	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
@@ -16,5 +22,14 @@ public class Clientes {
 	public Cliente salvar(Cliente cliente) {
 		jdbcTemplate.update(INSERT, new Object[] {cliente.getNome()});
 		return cliente;
+	}
+	
+	public List<Cliente> obterTodos() {
+		return JdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+			@Override
+			public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
+				return null;
+			}
+		});
 	}
 }
