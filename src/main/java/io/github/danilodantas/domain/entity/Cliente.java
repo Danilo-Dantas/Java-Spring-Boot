90f904cf511c1f15a9a5dd7e95aa262ba3f0dff7
Danilo <danilodantasofc@gmail.com>
2022-10-17 10:34:01 -0300

    Mapeando a entidade Cliente para JPA

--- src/main/java/io/github/danilodantas/domain/entity/Cliente.java
+++ src/main/java/io/github/danilodantas/domain/entity/Cliente.java
@@ -1,9 +1,22 @@
 package io.github.danilodantas.domain.entity;
 
-//CLASSE DE ENTIDADE REFERENTE A TABELA CLIENTE DO BANCO DE DADOS
-public class Cliente {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity //INDICA AO JPA QUE FAÇA O SCAN DESSA CLASSE(CLIENTE) E REGISTRE ELA COMO UMA TABELA DO BANCO DE DADOS
+@Table(name = "cliente")
+public class Cliente { //CLASSE DE ENTIDADE REFERENTE A TABELA CLIENTE DO BANCO DE DADOS
+	
+	@Id //IDENTIFICA A PRIMARY KEY DA TABELA
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")//CASO O NOME DA COLUNA ID SEJA DIFERENTE, PODEMOS USAR ESSA ANNOTATION, NESSE CASO É MEIO INUTIL, MAS COLOQUEI DE EXEMPLO
 	private Integer id;
+	
+	@Column(name = "nome", length = 100)
 	private String nome;
 
 	//CONTRUTORES
