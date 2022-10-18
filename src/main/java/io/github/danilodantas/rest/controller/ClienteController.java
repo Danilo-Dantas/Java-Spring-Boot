c48364f0f66f6c5d7e3a346f709516eeb4cc3ac9
Danilo <danilodantasofc@gmail.com>
2022-10-18 15:35:19 -0300

    Mapeando as Requisições com request mapping

--- src/main/java/io/github/danilodantas/rest/controller/ClienteController.java
+++ src/main/java/io/github/danilodantas/rest/controller/ClienteController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/api/clientes")
 public class ClienteController {
 	
-	@RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+	@RequestMapping(
+			value = { "/api/clientes/hello/{nome}", "/api/hello" },
+			method = RequestMethod.POST,
+			consumes = { "application/json", "application/xml" },
+			produces = { "application/json", "application/xml" }
+			)
 	@ResponseBody
 	public String helloCliente( @PathVariable("nome") String nomeCliente) {
 		return String.format("Bem vindo(a), %s! Essa é minha primeira API feita na vida =D", nomeCliente);
