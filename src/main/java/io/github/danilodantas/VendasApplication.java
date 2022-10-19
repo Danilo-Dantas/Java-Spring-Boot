d6f1fd825b2cd3b8315f8cf6061961eee8ed80a0
Danilo <danilodantasofc@gmail.com>
2022-10-19 13:45:04 -0300

    Delete Mapping: deletando um recurso no servidor

--- src/main/java/io/github/danilodantas/VendasApplication.java
+++ src/main/java/io/github/danilodantas/VendasApplication.java
@@ -1,26 +1,11 @@
 package io.github.danilodantas;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import io.github.danilodantas.domain.entity.Cliente;
-import io.github.danilodantas.domain.repository.Clientes;
 
 @SpringBootApplication
 public class VendasApplication {
 	
-	@Bean
-	public CommandLineRunner commandLineRunner(@Autowired Clientes clientes) {
-		return args -> {
-			Cliente c = new Cliente(null, "Fulano");
-			clientes.save(c);
-			
-			System.out.println(c);
-		};
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
--- src/main/java/io/github/danilodantas/rest/controller/ClienteController.java
+++ src/main/java/io/github/danilodantas/rest/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,20 @@ public class ClienteController {
 	public ResponseEntity<Cliente> save( @RequestBody Cliente cliente) {
 		Cliente clienteSalvo = clientes.save(cliente);
 		return ResponseEntity.ok(clienteSalvo);
+	}
+	
+	
+	//METODO DELETE
+	@DeleteMapping("/api/clientes/{id}")
+	@ResponseBody
+	public ResponseEntity<Cliente> delete(@PathVariable Integer id) {
+		Optional<Cliente> cliente = clientes.findById(id);
+		
+		if(cliente.isPresent()) {
+			clientes.delete( cliente.get());
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
 	}
 	
 }
