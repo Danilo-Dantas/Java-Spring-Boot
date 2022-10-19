f226c8f0e44612a50b9c986c8e098b54efde6241
Danilo <danilodantasofc@gmail.com>
2022-10-19 14:02:31 -0300

    Put Mapping - Atualizando um cliente

--- src/main/java/io/github/danilodantas/rest/controller/ClienteController.java
+++ src/main/java/io/github/danilodantas/rest/controller/ClienteController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.github.danilodantas.domain.entity.Cliente;
@@ -56,6 +58,17 @@ public class ClienteController {
 			return ResponseEntity.noContent().build();
 		}
 		return ResponseEntity.notFound().build();
+	}
+	
+	//METODO ATUALIZAR
+	@PutMapping("/api/clientes/{id}")
+	@ResponseBody
+	public ResponseEntity update( @PathVariable Integer id, @RequestBody Cliente cliente) {
+		return clientes.findById(id).map(clienteExistente -> {
+			cliente.setId(clienteExistente.getId());
+			clientes.save(cliente);
+			return ResponseEntity.noContent().build();
+		}).orElseGet( () -> ResponseEntity.notFound().build() );
 	}
 	
 }
