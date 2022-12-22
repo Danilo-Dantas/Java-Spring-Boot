3c882f76cc786903ffa32130af6d60bc02bbd1a2
Danilo <danilodantasofc@gmail.com>
2022-12-22 14:40:33 -0300

    Bean validation para obrigatoriedade do CPF e validacao do mesmo

--- src/main/java/io/github/danilodantas/domain/entity/Cliente.java
+++ src/main/java/io/github/danilodantas/domain/entity/Cliente.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import java.util.Set;
 
 @Data
@@ -23,10 +25,12 @@ public class Cliente {
     private Integer id;
 
     @Column(name = "nome", length = 100)
-    @NotEmpty(message = "Campo Nome é obrigatório")
+    @NotEmpty(message = "Campo Nome é obrigatório.")
     private String nome;
 
     @Column(name = "cpf", length = 11)
+    @NotEmpty(message = "Campo CPF é obrigatório.")
+    @CPF(message = "informe um CPF válido.")
     private String cpf;
 
     @JsonIgnore
