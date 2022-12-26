5b38c2ecaf2cfdbd4c0e076fb231a4a84a24ee3c
Danilo <danilodantasofc@gmail.com>
2022-12-26 11:59:52 -0300

    passowrd encode

--- src/main/java/io/github/danilodantas/config/SecurityConfig.java
+++ src/main/java/io/github/danilodantas/config/SecurityConfig.java
@@ -1,12 +1,31 @@
 package io.github.danilodantas.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		PasswordEncoder passwordEncoder = new PasswordEncoder() {
+			
+			@Override
+			public String encode(CharSequence rawPassword) {
+				return rawPassword + "321";
+			}
+			
+			@Override
+			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+				return (rawPassword + "321").equals(encodedPassword);
+			}
+			
+		};
+	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
