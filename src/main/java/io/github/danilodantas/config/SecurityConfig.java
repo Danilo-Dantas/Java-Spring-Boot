85d854521c2618c6fce8c940b2c6ce8f3716e6c2
Danilo <danilodantasofc@gmail.com>
2022-12-26 16:20:45 -0300

    Configurando a autorização de URLs

--- src/main/java/io/github/danilodantas/config/SecurityConfig.java
+++ src/main/java/io/github/danilodantas/config/SecurityConfig.java
@@ -23,6 +23,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
+		http.csrf().disable().authorizeRequests().antMatchers("/api/clientes/**").permitAll().and().formLogin();
 	}
 }
