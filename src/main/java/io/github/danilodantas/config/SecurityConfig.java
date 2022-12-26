c718817e24fdfef0ccbcf8a68dfe2bb281fbc0be
Danilo <danilodantasofc@gmail.com>
2022-12-26 11:32:05 -0300

    criação e configuração da classe Security Config

---
+++ src/main/java/io/github/danilodantas/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package io.github.danilodantas.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		super.configure(auth);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+	}
+}
