23e73ff2128266027c33ae1d82ffb6d793158a69
Danilo <danilodantasofc@gmail.com>
2022-12-26 17:26:09 -0300

    Configurando as Roles e Authorities

--- src/main/java/io/github/danilodantas/config/SecurityConfig.java
+++ src/main/java/io/github/danilodantas/config/SecurityConfig.java
@@ -10,19 +10,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser("fulano").password(passwordEncoder().encode("123")).roles("USER");
+		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser("fulano")
+				.password(passwordEncoder().encode("123")).roles("USER", "ADMIN");
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/api/clientes/**").permitAll().and().formLogin();
+		http.csrf().disable()
+		.authorizeRequests()
+			.antMatchers("/api/clientes/**")
+				.hasAnyRole("USER", "ADMIN")
+			.antMatchers("/api/pedidos/**")
+				.hasAnyRole("USER", "ADMIN")
+			.antMatchers("/api/produtos/**")
+				.hasRole("ADMIN")
+		.and()
+			.formLogin();
 	}
 }
