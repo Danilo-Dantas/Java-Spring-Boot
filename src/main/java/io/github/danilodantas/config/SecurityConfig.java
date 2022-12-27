58a1db47707ef69b6af18d2a637425f771491903
Danilo <danilodantasofc@gmail.com>
2022-12-27 09:58:17 -0300

    Implementação do UserDetailsService

--- src/main/java/io/github/danilodantas/config/SecurityConfig.java
+++ src/main/java/io/github/danilodantas/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.github.danilodantas.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,9 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import io.github.danilodantas.service.impl.UsuarioServiceImpl;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UsuarioServiceImpl usuarioService;
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -18,8 +24,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser("fulano")
-				.password(passwordEncoder().encode("123")).roles("USER", "ADMIN");
+		auth
+			.userDetailsService(usuarioService)
+			.passwordEncoder(passwordEncoder());
+			
+				
 	}
 
 	@Override
