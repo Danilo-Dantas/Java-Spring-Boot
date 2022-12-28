1836d3b836b5201f08f1c56ea751b0605f24fbc8
Danilo <danilodantasofc@gmail.com>
2022-12-28 10:37:28 -0300

    Fazendo a migração para o banco MySQL

--- src/main/java/io/github/danilodantas/domain/entity/Cliente.java
+++ src/main/java/io/github/danilodantas/domain/entity/Cliente.java
@@ -20,7 +20,7 @@ import java.util.Set;
 public class Cliente {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
--- src/main/java/io/github/danilodantas/domain/entity/Usuario.java
+++ src/main/java/io/github/danilodantas/domain/entity/Usuario.java
@@ -22,7 +22,7 @@ import lombok.NoArgsConstructor;
 public class Usuario {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	@Column
