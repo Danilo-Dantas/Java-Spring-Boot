2ea0ba254f9b5c4a1e1c5f64c0eae6a667d73a31
Danilo <danilodantasofc@gmail.com>
2022-12-28 12:04:57 -0300

    Configurando o Security no Swagger

--- src/main/java/io/github/danilodantas/config/SwaggerConfig.java
+++ src/main/java/io/github/danilodantas/config/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package io.github.danilodantas.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,8 +11,12 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -27,6 +35,8 @@ public class SwaggerConfig {
 						.basePackage("io.github.danilodantas.rest.controller"))
 				.paths(PathSelectors.any())
 				.build()
+				.securityContexts(Arrays.asList(securityContext()))
+				.securitySchemes(Arrays.asList(apiKey()))
 				.apiInfo(apiInfo());
 	}
 	
@@ -45,4 +55,23 @@ public class SwaggerConfig {
 				, "danilodantasofc@gmail.com");
 	}
 	
+	public ApiKey apiKey() {
+		return new ApiKey("JWT", "Authorization", "header");
+	}
+	
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth())
+				.forPaths(PathSelectors.any()).build();
+	}
+	
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope(
+				"global", "acessEverything");
+		AuthorizationScope[] scopes = new AuthorizationScope[1];
+		scopes[0] = authorizationScope;
+		SecurityReference reference = new SecurityReference("JWT", scopes);
+		List<SecurityReference> auths = new ArrayList<>();
+		auths.add(reference);
+		return auths;
+	}
 }
